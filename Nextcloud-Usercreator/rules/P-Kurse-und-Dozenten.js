5c0d3866d10d47d2a1dcb3c29500dac50888c974
Claudius Coenen <opensource@amenthes.de>
2020-04-02 04:05:06 +0200

    rule file updated to take advantage of new mover functionality

--- Nextcloud-Usercreator/rules/P-Kurse-und-Dozenten.js
+++ Nextcloud-Usercreator/rules/P-Kurse-und-Dozenten.js
@@ -28,7 +28,7 @@ module.exports = function (lists, config) {
 		{shareType: 1, shareWith: `Studierende-${course}`, permissions: 1}
 	];
 
-	for (let teamID = 1; teamID <= 8; teamID++) {
+	for (let teamID = 1; teamID <= 10; teamID++) {
 		admin.groups.push(`${course}-Team-${teamID}`);
 		admin.shares[`/${timecode}-${course}/${course} Team ${teamID} Intern`] = [
 			{shareType: 1, shareWith: `${course}-Team-${teamID}`, permissions: 31}
@@ -45,9 +45,14 @@ module.exports = function (lists, config) {
 		item.groups = [`Dozierende-${course}`];
 		item.quota = '10GB';
 		item.shares = {};
+
+		// these will just create directories.
+		item.shares[`/${timecode}-${course}`] = [];
+		item.shares[`/${timecode}-${course}/${item.veranstaltung} Abgaben`] = [];
+		item.shares[`/${timecode}-${course}/${course} Gruppenabgaben`] = [];
+
 		if (item.tutor !== 'tutor') { // tutors don't get their own directories!
-			item.shares[`/${timecode}-${course}`] = []; // this will just create a directory.
-			item.shares[`/${timecode}-${course}/` + item.veranstaltung + ' Unterlagen ' + item.name] = [
+			item.shares[`/${timecode}-${course}/${item.veranstaltung} Unterlagen`] = [
 				{shareType: 1, shareWith: `Dozierende-${course}`, permissions: 1},
 				{shareType: 1, shareWith: `Studierende-${course}`, permissions: 1}
 			];
@@ -64,7 +69,7 @@ module.exports = function (lists, config) {
 		item.shares = {};
 		item.shares[`/${timecode}-${course}`] = []; // this will just create a directory.
 		for (let v in veranstaltungen) {
-			let s = item.shares[`/${timecode}-${course}/${v} Upload ${item.name}`] = [];
+			let s = item.shares[`/${timecode}-${course}/${item.name} Abgabe ${v}`] = [];
 			veranstaltungen[v].forEach(dozent => {
 				s.push({shareType: 0, shareWith: dozent, permissions: 1});
 			});
