febbd82ba39934f74bfe225992e516ad30fae3f3
Claudius <opensource@amenthes.de>
2020-04-11 01:26:02 +0200

    minor clarification to votersPerOption

--- Elective-Poll-Selection-Conduit/index.js
+++ Elective-Poll-Selection-Conduit/index.js
@@ -7,32 +7,36 @@ const sortingHat = require('./lib/sorting-hat');
 const filename = process.argv[2];
 console.log(`Reading file ${filename}`);
 const data = reader.parseHTML(fs.readFileSync(filename, 'UTF-8'));
-reader.applyVoterMaximumPerOption(data.options, require('./votersPerCourse'));
-reader.resolveOptionNames(data);
 console.log(`${data.options.length} options found / ${data.voters.length} voters found`);
+
+reader.applyVoterMaximumPerOption(data.options, require('./votersPerOption'));
+reader.resolveOptionNames(data);
 
 calculator.countOptionPopularity(data.voters);
 calculator.prepareChoicesByPriority(data.voters);
 
-// print sorted by descending popularity
-data.options.sort((a, b) => b.popularity-a.popularity);
+console.log('\n\n# Popularity');
+data.options.sort((a, b) => b.popularity - a.popularity);
 data.options.forEach((o) => {
-	console.log(o);
+	console.log(`* ${o.name} (${o.popularity}/${o.maximumAssignableVoters})`);
 });
 
-console.log('# Voters');
+/*
+// Nobody cares. Interesting for debugging, though.
+console.log('\n\n# Voters including Choices');
 data.voters.forEach((v) => {
 	console.log(`- ${v.toString()}`);
 });
+*/
 
-console.log('\n\n# chosen choices');
+console.log('\n\n# Assigned choices');
 const randomizedList = sortingHat.predictableRandomizer(data.voters, '1');
 sortingHat.assign(data.options, randomizedList);
 randomizedList.forEach((v) => {
 	console.log(`- ${v.userid}: ${v.assignedOptions.map(o=>o.name).join(', ')} happiness: ${Math.round(v.happiness/v.maximumAssignableOptions*100)}%`);
 });
 
-console.log('\n\n# courses');
+console.log('\n\n# Course Summary');
 data.options.forEach((o) => {
 	console.log(`${o.name}: ${o.assignedVoters.length} people`);
 });
