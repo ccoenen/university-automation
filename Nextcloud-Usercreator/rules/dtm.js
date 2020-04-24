aa6a062627ddd9f64d75622359df04cddbf01dd8
Claudius Coenen <opensource@amenthes.de>
2020-04-24 20:53:08 +0200

    adding dtm rules file

---
+++ Nextcloud-Usercreator/rules/dtm.js
@@ -0,0 +1,65 @@
+const timecode = process.env.TIMECODE; // "2019-SS";
+const title = process.env.ELECTIVETITLE; // "Physical Interaction";
+const GROUPNAME = `${title}`;
+
+if (!timecode || !title) {
+	console.error("please set TIMECODE and ELECTIVETITLE env vars!");
+	process.exit(1);
+}
+
+if (title.indexOf('/') !== -1) {
+	console.error("title cannot contain \"/\"!");
+	process.exit(1);
+}
+
+function usersAndShares(lists /*, config */) {
+	var users = [];
+
+	lists[0].forEach((item, index) => {
+		item.groups = [
+			GROUPNAME
+		];
+		item.quota = '10GB';
+		item.shares = {};
+
+		// first one creates the share and shares it with everyone else in the teaching staff
+		if (index === 0) {
+			// /2020-SS-DTM2 Unterlagen
+			item.shares[`/${timecode}-${title} Unterlagen`] = [
+				{shareType: 1, shareWith: GROUPNAME, permissions: 1}
+			].concat(lists[0].map((i) => {return {shareType: 0, shareWith: i.userid, permissions: 1};}));
+		}
+		// /2020-SS-DTM2 Abgaben
+		item.shares[`/${timecode}-${title} Abgaben`] = [];
+
+		users.push(item);
+	});
+
+	lists[1].forEach((item) => {
+		item.groups = [GROUPNAME];
+		item.shares = {};
+		// /2020-SS-DTM2 Bernd Brot Abgabe
+		item.shares[`/${timecode}-${title} ${item.name} Abgabe`] = lists[0].map(i => {
+			return {shareType: 0, shareWith: i.userid, permissions: 1}
+		});
+
+		users.push(item);
+	});
+
+	return users;
+}
+
+const STUDENT_UPLOAD_REGEX = new RegExp(`^${timecode}-${title} .+ Abgabe$`);
+
+const moveInstructions = [
+	// format: search pattern, target directory
+
+	// teachers: base level will have all the individual upload dirs from students
+	{ pattern: STUDENT_UPLOAD_REGEX, target: `/${timecode}-${title} Abgaben/` },
+];
+
+
+module.exports = {
+	usersAndShares,
+	moveInstructions
+};
