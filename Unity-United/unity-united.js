fef8188e090b9cc2117e6b1f857d1202fd12770a
Claudius Coenen <opensource@amenthes.de>
2025-02-16 16:54:11 +0100

    reading the version from the version file

--- Unity-United/unity-united.js
+++ Unity-United/unity-united.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 const DEFAULT_UNITY_VERISON = "2022.3.55f1";
+const VERSION_REGEX = /^m_EditorVersion:\s(?<version>.+)[\r\n]+|$/;
 
 const TEMPLATE = {
 	"title": "Example Project",
@@ -37,24 +38,36 @@ function findProjectVesionPath(parent, list = []) {
 }
 
 
-function projectVersionPathToProject(VersionFile) {
-	console.log(VersionFile);
+function projectVersionPathToProject(versionFile) {
+	console.log(versionFile);
 	const project = structuredClone(TEMPLATE);
 
-	const projectPath = path.join(VersionFile, "..", "..");
+	const version = extractVersion(versionFile, project);
+
+	const projectPath = path.join(versionFile, "..", "..");
 	project.title = path.basename(projectPath);
 	project.path = projectPath;
 	project.containingFolderPath = path.join(projectPath, "..");
+	project.version = version || DEFAULT_UNITY_VERISON;
 
 	return project;
 }
 
+function extractVersion(versionFile, project) {
+	const versionContent = fs.readFileSync(versionFile, "UTF-8");
+	const versionMatch = VERSION_REGEX.exec(versionContent);
+	return versionMatch ? versionMatch.groups.version : null;
+}
 
+
+
+// finding and outputting.
 const paths = findProjectVesionPath(parentDirectory, []);
-paths.forEach(p => console.log);
+// paths.forEach(p => console.log);
 const projects = paths.map(projectVersionPathToProject);
 const projectObject = {};
 for (const p of projects) {
 	projectObject[p.path] = p;
 }
+
 console.log(JSON.stringify(projectObject, null, "\t"));
